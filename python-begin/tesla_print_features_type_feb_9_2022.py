40b7908fbbf8a5470de353e76193ee4b86586668
tariq <tariqrahiman@hotmail.com>
2022-02-09 15:24:17 -0800

    Sample program to Print the Type of variables - Tariq Rahiman

---
+++ python-begin/tesla_print_features_type_feb_9_2022.py
@@ -0,0 +1,42 @@
+"""
+@author : Tariq Rahiman
+@date : 2-9-2022
+Sample program to Print the Type of variables for Tesla Model 3 Features
+"""
+
+base_price = 35400
+range_EPA = "220 miles"
+efficiency = "26 kWh/100 miles"
+MPGe = "131 miles"
+battery_capacity = "54 kWh"
+dc_charging = "130 miles"
+ac_charging = "29.5 miles"
+full_ac_charge_time = "6 hours"
+powertrain = "Single-motor rear-wheel drive"
+motor = "Interior Permanent Magnet (IPM) synchronous machine"
+power_peak = "211 kW"
+power_to_weight = "131 W/kg"
+torque = "450 N⋅m (330 lb⋅ft)"
+acceleration = "0–60 mph"
+top_speed = "130 mph"
+autonomous_capability = "Autopilot including TACC, autosteer, lane-keeping, lane-changing, active-safety autonomy.[232][249] (All vehicles include all hardware needed for \"Full Self-Driving\", including 1 radar, 8 cameras, 12 sonar, and AI computer"
+display = "Single center-mounted WUXGA 15.4-inch (39 cm) LCD touchscreen in landscape orientation that combines the instrument cluster and infotainment"
+entry = "Keyless NFC keycard and Bluetooth Low Energy smartphone connection for vehicle access;[258][259] optional key fob"
+
+print(type(base_price))
+print(type(range_EPA))
+print(type(efficiency))
+print(type(MPGe))
+print(type(battery_capacity))
+print(type(dc_charging))
+print(type(ac_charging))
+print(type(full_ac_charge_time))
+print(type(powertrain))
+print(type(motor))
+print(type(power_peak))
+print(type(torque))
+print(type(acceleration))
+print(type(top_speed))
+print(type(autonomous_capability))
+print(type(display))
+print(type(entry))
