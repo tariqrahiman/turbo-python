839767ed526b3b3549eaf0e93ba76049d37d633a
tariq <tariqrahiman@hotmail.com>
2022-02-09 15:19:07 -0800

    Printing Tesla Model 3 Features - Tariq Rahiman

---
+++ python-begin/tesla_print_features_feb_9_2022.py
@@ -0,0 +1,43 @@
+"""
+@author : Tariq Rahiman
+@date : 2-9-2022
+Sample Print statements for Tesla Model 3 Features
+use \ to escape " within strings. Example \"
+"""
+
+base_price = 35400
+range_EPA = "220 miles"
+efficiency = "26 kWh/100 miles"
+MPGe = "131 miles"
+battery_capacity = "54 kWh"
+dc_charging = "130 miles"
+ac_charging = "29.5 miles"
+full_ac_charge_time = "6 hours"
+powertrain = "Single-motor rear-wheel drive"
+motor = "Interior Permanent Magnet (IPM) synchronous machine"
+power_peak = "211 kW"
+power_to_weight = "131 W/kg"
+torque = "450 N⋅m (330 lb⋅ft)"
+acceleration = "0–60 mph"
+top_speed = "130 mph"
+autonomous_capability = "Autopilot including TACC, autosteer, lane-keeping, lane-changing, active-safety autonomy.[232][249] (All vehicles include all hardware needed for \"Full Self-Driving\", including 1 radar, 8 cameras, 12 sonar, and AI computer"
+display = "Single center-mounted WUXGA 15.4-inch (39 cm) LCD touchscreen in landscape orientation that combines the instrument cluster and infotainment"
+entry = "Keyless NFC keycard and Bluetooth Low Energy smartphone connection for vehicle access;[258][259] optional key fob"
+
+print(base_price)
+print(range_EPA)
+print(efficiency)
+print(MPGe)
+print(battery_capacity)
+print(dc_charging)
+print(ac_charging)
+print(full_ac_charge_time)
+print(powertrain)
+print(motor)
+print(power_peak)
+print(torque)
+print(acceleration)
+print(top_speed)
+print(autonomous_capability)
+print(display)
+print(entry)
